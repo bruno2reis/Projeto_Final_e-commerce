949cd447afb1a6c1af8b8b7e8095d20a7470cc3e
Bruno <brunodosreis21@gmail.com>
2018-03-21 23:43:13 -0300

    bruno-modelagem de index

---
+++ nav.php
@@ -0,0 +1,20 @@
+
+    <div class="container">
+        <img id="iconeLogo" src="img/logo.jpg"/>
+        <div id="divBusca">
+          <input type="text" id="txtBusca" placeholder="Buscar..."/>
+          <button id="btnBusca">Buscar</button>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <nav id="menu">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Produtos</a></li>
+                <li><a href="#">Missão</a></li>
+                <li><a href="#">Links</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+    </div>
