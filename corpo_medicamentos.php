058cba628e2fd2d484840d38e93178fe4e735904
fprocaci <fprocaci@gmail.com>
2018-04-02 21:09:28 -0300

    pags de categorias

---
+++ corpo_medicamentos.php
@@ -0,0 +1,120 @@
+<section class="container p-t-3">
+	<div class="row">
+		<div class="col-lg-12">
+			<h3><span style="color: rgb(38,95,167)">Medicamentos</span></h3>
+		</div>
+	</div>
+</section>
+
+
+	<!-- Lista de produto -->
+		
+	<div class="container" style="padding-bottom:0">
+		  <div class="row">	
+	
+			<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-4">
+				<div class="bloco">
+					<div class="text-center">				
+						<img class="thumb_prod" src="img/novalgina.png" width="150" alt="Novalgina">						
+						<div class="card-title">Novalgina 1g 10 Comprimidos</div>
+						<div class="card-subtitle mb-2 text-muted">Sanofi Aventis</div>
+						<div class="card-text">R$ 15,00</div>								
+						<br>
+						<a href="" class="btn btn-primary">Adicionar ao carrinho</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-4">
+				<div class="bloco">
+					<div class="text-center">				
+						<img class="thumb_prod" src="img/novalgina.png" width="150" alt="Novalgina">						
+						<div class="card-title">Novalgina 1g 10 Comprimidos</div>
+						<div class="card-subtitle mb-2 text-muted">Sanofi Aventis</div>
+						<div class="card-text">R$ 15,00</div>								
+						<br>
+						<a href="corpo_total.php" class="btn btn-primary">Adicionar ao carrinho</a>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-4">
+				<div class="bloco">
+					<div class="text-center">				
+						<img class="thumb_prod" src="img/novalgina.png" width="150" alt="Novalgina">						
+						<div class="card-title">Novalgina 1g 10 Comprimidos</div>
+						<div class="card-subtitle mb-2 text-muted">Sanofi Aventis</div>
+						<div class="card-text">R$ 15,00</div>								
+						<br>
+						<a href="corpo_total.php" class="btn btn-primary">Adicionar ao carrinho</a>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-4">
+				<div class="bloco">
+					<div class="text-center">				
+						<img class="thumb_prod" src="img/novalgina.png" width="150" alt="Novalgina">						
+						<div class="card-title">Novalgina 1g 10 Comprimidos</div>
+						<div class="card-subtitle mb-2 text-muted">Sanofi Aventis</div>
+						<div class="card-text">R$ 15,00</div>								
+						<br>
+						<a href="corpo_total.php" class="btn btn-primary">Adicionar ao carrinho</a>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-4">
+				<div class="bloco">
+					<div class="text-center">				
+						<img class="thumb_prod" src="img/novalgina.png" width="150" alt="Novalgina">						
+						<div class="card-title">Novalgina 1g 10 Comprimidos</div>
+						<div class="card-subtitle mb-2 text-muted">Sanofi Aventis</div>
+						<div class="card-text">R$ 15,00</div>								
+						<br>
+						<a href="corpo_total.php" class="btn btn-primary">Adicionar ao carrinho</a>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-4">
+				<div class="bloco">
+					<div class="text-center">				
+						<img class="thumb_prod" src="img/novalgina.png" width="150" alt="Novalgina">						
+						<div class="card-title">Novalgina 1g 10 Comprimidos</div>
+						<div class="card-subtitle mb-2 text-muted">Sanofi Aventis</div>
+						<div class="card-text">R$ 15,00</div>								
+						<br>
+						<a href="corpo_total.php" class="btn btn-primary">Adicionar ao carrinho</a>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-4">
+				<div class="bloco">
+					<div class="text-center">				
+						<img class="thumb_prod" src="img/novalgina.png" width="150" alt="Novalgina">						
+						<div class="card-title">Novalgina 1g 10 Comprimidos</div>
+						<div class="card-subtitle mb-2 text-muted">Sanofi Aventis</div>
+						<div class="card-text">R$ 15,00</div>								
+						<br>
+						<a href="corpo_total.php" class="btn btn-primary">Adicionar ao carrinho</a>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+				<div class="bloco">
+					<div class="text-center">				
+						<img class="thumb_prod" src="img/novalgina.png" width="150" alt="Novalgina">						
+						<div class="card-title">Novalgina 1g 10 Comprimidos</div>
+						<div class="card-subtitle mb-2 text-muted">Sanofi Aventis</div>
+						<div class="card-text">R$ 15,00</div>								
+						<br>
+						<a href="corpo_total.php" class="btn btn-primary">Adicionar ao carrinho</a>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
